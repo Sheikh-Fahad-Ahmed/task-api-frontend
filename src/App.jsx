6165bf5b8b90e313d9128004b9e758d67c0117ce
Fahad Ahmed <fahad838@gmail.com>
2026-06-25 15:13:26 +0530

    Feat: create compo ReaultView and Task to view state variables

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ function App() {
   const [tasks, setTask] = useState([]);
 
   function handleTasks(task) {
-    setTask([...tasks], task);
+    setTask((tasks) => [...tasks, task]);
   }
+
   return (
     <div>
-      <Main />
+      <Main onAddTask={handleTasks} />
+      <ResultViw tasks={tasks} />
     </div>
   );
 }
 
-function Main() {
+function Main({ onAddTask }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [status, setStatus] = useState("");
@@ -23,19 +25,55 @@ function Main() {
     if (!title) return;
 
     const newTask = { title, description, status };
+    onAddTask(newTask);
+    setTitle("");
+    setDescription("");
   }
 
   return (
     <form action="" onSubmit={handleSubmit}>
-      <input type="text" name="title" id="" placeholder="title" />
-      <input type="text" name="description" id="" placeholder="description" />
-      <select name="status" id="">
+      <input
+        type="text"
+        name="title"
+        id=""
+        placeholder="title"
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        name="description"
+        id=""
+        placeholder="description"
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <select name="status" onChange={(e) => setStatus(e.target.value)}>
         <option value="pending">pending</option>
         <option value="in-progress">in-progress</option>
         <option value="done">done</option>
       </select>
       <button>Send</button>
     </form>
+  );
+}
+
+function ResultViw({ tasks }) {
+  return (
+    <div>
+      <h2>Result</h2>
+      {tasks.map((task) => (
+        <Task task={task} key={task.title} />
+      ))}
+    </div>
+  );
+}
+
+function Task({ task }) {
+  return (
+    <div>
+      <p>title: {task.title}</p>
+      <p>description: {task.description}</p>
+      <p>status: {task.status}</p>
+    </div>
   );
 }
 export default App;
