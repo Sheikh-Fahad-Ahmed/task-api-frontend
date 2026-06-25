b573c1bf06128801d1de260cab372d2120185d08
Fahad Ahmed <fahad838@gmail.com>
2026-06-25 14:53:49 +0530

    Feat: add state variable and create handleTask()

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 function App() {
+  const [tasks, setTask] = useState([]);
+
+  function handleTasks(task) {
+    setTask([...tasks], task);
+  }
   return (
     <div>
       <Main />
@@ -7,16 +14,27 @@ function App() {
 }
 
 function Main() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [status, setStatus] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!title) return;
+
+    const newTask = { title, description, status };
+  }
+
   return (
-    <form action="">
+    <form action="" onSubmit={handleSubmit}>
       <input type="text" name="title" id="" placeholder="title" />
       <input type="text" name="description" id="" placeholder="description" />
       <select name="status" id="">
         <option value="pending">pending</option>
-        <br />
         <option value="in-progress">in-progress</option>
         <option value="done">done</option>
       </select>
+      <button>Send</button>
     </form>
   );
 }
