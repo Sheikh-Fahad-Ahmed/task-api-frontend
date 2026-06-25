4f19d4fe0efcc3ab3a6f3eaf47cacfdeab7b7f71
Fahad Ahmed <fahad838@gmail.com>
2026-06-25 14:50:06 +0530

    Feat: create Main() to accept task fields that have state variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 function App() {
   return (
     <div>
-      <h1>Hello, World</h1>
+      <Main />
     </div>
   );
 }
 
+function Main() {
+  return (
+    <form action="">
+      <input type="text" name="title" id="" placeholder="title" />
+      <input type="text" name="description" id="" placeholder="description" />
+      <select name="status" id="">
+        <option value="pending">pending</option>
+        <br />
+        <option value="in-progress">in-progress</option>
+        <option value="done">done</option>
+      </select>
+    </form>
+  );
+}
 export default App;
