3f3e0f1544bb8f2e6661c8f342010ae2ec790840
Fahad Ahmed <fahad838@gmail.com>
2026-06-29 16:54:36 +0530

    Feat: refactor App.jsx to include homepage

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+// import { useEffect } from "react";
+// import Home from "./Home";
+// import Tasks from "./TaskList";
+import HomePage from "./HomePage";
+
+function App() {
+  return <HomePage />;
+}
+
+export default App;
